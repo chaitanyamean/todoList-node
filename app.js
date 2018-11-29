413f4c24d91bc5a9de748e86f1fdaf9e92de9cf6
ioskyc <Hack2Hire23@gmail.com>
2018-11-30 01:21:35 +0530

    bcrypt added

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(globalErrorMiddleware.globalNotFoundHandler);
 
 const server = http.createServer(app);
 // start listening to http server
-console.log(appConfig);
+console.log('sdsd',port);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);

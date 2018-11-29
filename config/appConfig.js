cd24c856b16ffbfa2c2615fe8628a22c413d0608
ioskyc <Hack2Hire23@gmail.com>
2018-11-30 00:21:22 +0530

    todoListDataBase added

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,7 +4,7 @@ appConfig.port = 3000;
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "dev";
 appConfig.db = {
-    uri: 'mongodb://127.0.0.1:27017/todolist'
+    uri: 'mongodb://127.0.0.1:27017/todoListDataBase'
   }
 appConfig.apiVersion = '/api/v1';
 
